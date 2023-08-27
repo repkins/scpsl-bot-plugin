b9b3094c4e63241fb35587d23e3da45f74528aa4
Antons Repins <dumperix1993@gmail.com>
2023-08-28 02:16:38 +0300

    disconnect wip

---
+++ Commands/Navigation/NavNodeDisconnectCommand.cs
@@ -0,0 +1,93 @@
+﻿using CommandSystem;
+using MapGeneration;
+using PlayerRoles;
+using RemoteAdmin;
+using SCPSLBot.Navigation.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SCPSLBot.Commands.Navigation
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    internal class NavNodeDisconnectCommand : ICommand
+    {
+        public string Command { get; } = "nav_node_disconnect";
+
+        public string[] Aliases { get; } = new string[] { };
+
+        public string Description { get; } = "Disconnects nearest node to target node.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!(sender is PlayerCommandSender playerCommandSender))
+            {
+                response = "You must be in-game to use this command!";
+                return false;
+            }
+
+            if (!playerCommandSender.ReferenceHub.IsAlive())
+            {
+                response = "Command disabled when you are not alive!";
+                return false;
+            }
+
+            var playerPosition = playerCommandSender.ReferenceHub.transform.position;
+            var node = NavigationGraph.Instance.FindNearestNode(playerPosition);
+            if (node == null)
+            {
+                response = $"No nearby node found at position {playerPosition}.";
+                return false;
+            }
+
+            Node targetNode = null;
+
+            var (roomName, roomShape) = node.RoomNameShape;
+            var room = RoomIdentifier.AllRoomIdentifiers.First(r => r.Name == roomName && r.Shape == roomShape);
+
+            if (arguments.Count > 1)
+            {
+                if (!int.TryParse(arguments.At(0), out var nodeId))
+                {
+                    response = $"Target node id should be valid integer.";
+                    return false;
+                }
+
+                if (!NavigationGraph.Instance.NodesByRoom[node.RoomNameShape].TryGet(nodeId, out targetNode))
+                {
+                    response = $"No target node exists at index {nodeId}.";
+                    return false;
+                }
+            }
+            else
+            {
+                var localPosition = room.transform.InverseTransformPoint(playerPosition);
+                var localForward = room.transform.InverseTransformDirection(playerCommandSender.ReferenceHub.transform.forward);
+
+                targetNode = NavigationGraphEditor.Instance.FindClosestNodeFacingAt(node.RoomNameShape, localPosition, localForward);
+
+                if (targetNode == null)
+                {
+                    response = $"No target node at player direction.";
+                    return false;
+                }
+            }
+
+            if (!node.ConnectedNodes.Contains(targetNode))
+            {
+                response = $"Node #{node.Id} is not connected with node {targetNode.Id}.";
+                return false;
+            }
+
+            node.ConnectedNodes.Remove(targetNode);
+            node.ConnectedNodeIndices.Remove(targetNode.Id);
+
+            response = $"Node #{node.Id} disconnected with node {targetNode.Id}.";
+
+            return true;
+        }
+    }
+}
--- Navigation/Graph/NavigationGraphEditor.cs
+++ Navigation/Graph/NavigationGraphEditor.cs
@@ -4,6 +4,7 @@ using MEC;
 using Mirror;
 using PluginAPI.Core;
 using PluginAPI.Events;
+using RemoteAdmin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +24,7 @@ namespace SCPSLBot.Navigation.Graph
         public Player PlayerEditing { get; set; }
         public Node LastEditingNode { get; set; }
         public Dictionary<Node, PrimitiveObjectToy> NodeVisuals { get; } = new Dictionary<Node, PrimitiveObjectToy>();
-        public Dictionary<(Node, Node), PrimitiveObjectToy> NodeConnectionVisuals { get; } = new Dictionary<(Node, Node), PrimitiveObjectToy>();
+        public Dictionary<(Node From, Node To), PrimitiveObjectToy> NodeConnectionVisuals { get; } = new Dictionary<(Node, Node), PrimitiveObjectToy>();
         public Dictionary<(Node, Node), PrimitiveObjectToy> NodeConnectionOriginVisuals { get; } = new Dictionary<(Node, Node), PrimitiveObjectToy>();
 
         public void Init()
@@ -31,6 +32,19 @@ namespace SCPSLBot.Navigation.Graph
             Timing.RunCoroutine(RunNodeInfoVisuals());
             Timing.RunCoroutine(RunNodeVisuals());
         }
+
+        public Node FindClosestNodeFacingAt((RoomName, RoomShape) roomNameShape, Vector3 localPosition, Vector3 localDirection)
+        {
+            var targetNode = NavigationGraph.Instance.NodesByRoom[roomNameShape]
+                .Select(n => (n, d: Vector3.SqrMagnitude(n.LocalPosition - localPosition)))
+                .Where(t => t.d < 50f && t.d > 1f)
+                .OrderBy(t => t.d)
+                .Select(t => t.n)
+                .FirstOrDefault(n => Vector3.Dot(Vector3.Normalize(n.LocalPosition - localPosition), localDirection) > 0.999848f);
+
+            return targetNode;
+        }
+
         public IEnumerator<float> RunNodeInfoVisuals()
         {
             while (true)
@@ -81,7 +95,23 @@ namespace SCPSLBot.Navigation.Graph
         {
             if (PlayerEditing != null)
             {
-                var primPrefab = _primPrefab;
+                var primPrefab = NetworkClient.prefabs.Values.Select(p => p.GetComponent<PrimitiveObjectToy>()).First(p => p);
+
+                foreach (var ((from, to), visual) in NodeConnectionVisuals.Select(p => (p.Key, p.Value)).ToArray())
+                {
+                    if (!from.ConnectedNodes.Contains(to) && !to.ConnectedNodes.Contains(from))
+                    {
+                        NetworkServer.Destroy(visual.gameObject);
+                        NodeConnectionVisuals.Remove((from, to));
+                    }
+
+                    if (!from.ConnectedNodes.Contains(to))
+                    {
+                        var connectionVisual = NodeConnectionOriginVisuals[(from, to)];
+                        NetworkServer.Destroy(connectionVisual.gameObject);
+                        NodeConnectionOriginVisuals.Remove((from, to));
+                    }
+                }
 
                 foreach (var node in NavigationGraph.NodesByRoom.Values.SelectMany(l => l))
                 {
@@ -94,7 +124,7 @@ namespace SCPSLBot.Navigation.Graph
                         NetworkServer.Spawn(visual.gameObject);
 
                         visual.transform.position = room.transform.TransformPoint(node.LocalPosition);
-                        visual.transform.localScale = Vector3.one * 0.25f;
+                        visual.transform.localScale = -Vector3.one * 0.25f;
                         visual.NetworkMaterialColor = Color.yellow;
 
                         NodeVisuals.Add(node, visual);
@@ -102,40 +132,48 @@ namespace SCPSLBot.Navigation.Graph
 
                     foreach (var connectedNode in node.ConnectedNodes)
                     {
-                        if (NodeConnectionVisuals.TryGetValue((connectedNode, node), out var inConnectionVisual))
-                        {
-                            if (NodeConnectionOriginVisuals.TryGetValue((connectedNode, node), out var connectionOriginVisual))
-                            {
-                                inConnectionVisual.NetworkMaterialColor = Color.yellow;
-
-                                NodeConnectionOriginVisuals.Remove((connectedNode, node));
-                                NetworkServer.Destroy(connectionOriginVisual.gameObject);
-                            }
-
-                            continue;
-                        }
-
                         if (!NodeConnectionVisuals.TryGetValue((node, connectedNode), out var outConnectionVisual))
                         {
-                            outConnectionVisual = UnityEngine.Object.Instantiate(primPrefab);
-                            outConnectionVisual.NetworkPrimitiveType = PrimitiveType.Cylinder;
-                            outConnectionVisual.transform.position = room.transform.TransformPoint(Vector3.Lerp(node.LocalPosition, connectedNode.LocalPosition, 0.5f));
-                            outConnectionVisual.transform.LookAt(room.transform.TransformPoint(connectedNode.LocalPosition));
-                            outConnectionVisual.transform.Rotate(outConnectionVisual.transform.forward, 90f);
-                            outConnectionVisual.transform.localScale = -Vector3.forward * 0.1f + -Vector3.right * 0.1f;
-                            outConnectionVisual.transform.localScale += -Vector3.up * Vector3.Distance(node.LocalPosition, connectedNode.LocalPosition) * 0.5f;
+                            if (!NodeConnectionVisuals.TryGetValue((connectedNode, node), out var inConnectionVisual))
+                            {
+                                outConnectionVisual = UnityEngine.Object.Instantiate(primPrefab);
+                                outConnectionVisual.NetworkPrimitiveType = PrimitiveType.Cylinder;
+                                outConnectionVisual.transform.position = room.transform.TransformPoint(Vector3.Lerp(node.LocalPosition, connectedNode.LocalPosition, 0.5f));
+                                outConnectionVisual.transform.LookAt(room.transform.TransformPoint(connectedNode.LocalPosition));
+                                outConnectionVisual.transform.RotateAround(outConnectionVisual.transform.position, outConnectionVisual.transform.right, 90f);
+                                outConnectionVisual.transform.localScale = -Vector3.forward * 0.1f + -Vector3.right * 0.1f;
+                                outConnectionVisual.transform.localScale += -Vector3.up * Vector3.Distance(node.LocalPosition, connectedNode.LocalPosition) * 0.5f;
+                                NetworkServer.Spawn(outConnectionVisual.gameObject);
+
+                                NodeConnectionVisuals.Add((node, connectedNode), outConnectionVisual);
+                            }
+                            else
+                            {
+                                outConnectionVisual = inConnectionVisual;
+                            }
+                        }
+
+                        if (!NodeConnectionOriginVisuals.TryGetValue((node, connectedNode), out var outConnectionOriginVisual))
+                        {
+                            outConnectionOriginVisual = UnityEngine.Object.Instantiate(primPrefab);
+                            outConnectionOriginVisual.transform.position = room.transform.TransformPoint(node.LocalPosition);
+                            outConnectionOriginVisual.transform.position += room.transform.TransformDirection(Vector3.Normalize(connectedNode.LocalPosition - node.LocalPosition)) * 0.125f;
+                            outConnectionOriginVisual.transform.localScale = -Vector3.one * 0.2f;
+                            NetworkServer.Spawn(outConnectionOriginVisual.gameObject);
+
+                            NodeConnectionOriginVisuals.Add((node, connectedNode), outConnectionOriginVisual);
+                        }
+
+                        if (NodeConnectionOriginVisuals.TryGetValue((connectedNode, node), out var inConnectionOriginVisual))
+                        {
+                            outConnectionVisual.NetworkMaterialColor = Color.yellow;
+                            outConnectionOriginVisual.NetworkMaterialColor = Color.yellow;
+                            inConnectionOriginVisual.NetworkMaterialColor = Color.yellow;
+                        }
+                        else
+                        {
                             outConnectionVisual.NetworkMaterialColor = Color.white;
-                            NetworkServer.Spawn(outConnectionVisual.gameObject);
-
-                            var connectionOriginVisual = UnityEngine.Object.Instantiate(primPrefab);
-                            connectionOriginVisual.transform.position = outConnectionVisual.transform.position;
-                            connectionOriginVisual.transform.position += outConnectionVisual.transform.up * (outConnectionVisual.transform.localScale.y + 0.125f);
-                            connectionOriginVisual.transform.localScale = Vector3.one * 0.2f;
-                            connectionOriginVisual.NetworkMaterialColor = Color.white;
-                            NetworkServer.Spawn(connectionOriginVisual.gameObject);
-
-                            NodeConnectionVisuals.Add((node, connectedNode), outConnectionVisual);
-                            NodeConnectionOriginVisuals.Add((node, connectedNode), connectionOriginVisual);
+                            outConnectionOriginVisual.NetworkMaterialColor = Color.white;
                         }
                     }
                 }
@@ -162,13 +200,9 @@ namespace SCPSLBot.Navigation.Graph
             }
         }
 
-        private PrimitiveObjectToy _primPrefab;
-
         #region Private constructor
         private NavigationGraphEditor()
-        {
-            _primPrefab = NetworkClient.prefabs.Values.Select(p => p.GetComponent<PrimitiveObjectToy>()).First(p => p);
-        }
+        { }
         #endregion
     }
 }
