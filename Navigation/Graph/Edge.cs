2a4249b4810872f9065c36be39b09260fd0e9664
Antons Repins <dumperix1993@gmail.com>
2023-12-19 22:14:21 +0200

    edge classes

---
+++ Navigation/Graph/Edge.cs
@@ -0,0 +1,26 @@
+﻿using PluginAPI.Core.Zones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SCPSLBot.Navigation.Graph
+{
+    internal class Edge
+    {
+        public RoomKindEdge RoomKindEdge { get; }
+        public FacilityRoom Room { get; }
+
+        public Vector3 Position => Room.Transform.TransformPoint(RoomKindEdge.LocalPosition);
+        public Vector3 Direction => Room.Transform.TransformDirection(RoomKindEdge.LocalDirection);
+        public float Extents => RoomKindEdge.Extents;
+
+        public Edge(RoomKindEdge roomKindEdge, FacilityRoom room)
+        {
+            RoomKindEdge = roomKindEdge;
+            Room = room;
+        }
+    }
+}
---
+++ Navigation/Graph/RoomKindEdge.cs
@@ -0,0 +1,19 @@
+﻿using MapGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SCPSLBot.Navigation.Graph
+{
+    internal class RoomKindEdge
+    {
+        public (RoomName, RoomShape, RoomZone) RoomKind { get; set; }
+
+        public Vector3 LocalPosition { get; set; }
+        public Vector3 LocalDirection { get; set; }
+        public float Extents { get; set; }
+    }
+}
