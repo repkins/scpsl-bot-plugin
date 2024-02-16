7f393c79c90f6e874d3def4cc9c8f8dc3b576a22
Antons Repins <dumperix1993@gmail.com>
2024-02-17 00:48:00 +0200

    keycard of permissions belief decorator

--- AI/FirstPersonControl/Mind/Beliefs/Item/Keycard/KeycardWithinPickupDistance.cs
+++ AI/FirstPersonControl/Mind/Beliefs/Item/Keycard/KeycardWithinPickupDistance.cs
@@ -1,49 +1,13 @@
 ﻿using Interactables.Interobjects.DoorUtils;
-using InventorySystem;
-using InventorySystem.Items.Keycards;
-using InventorySystem.Items.Pickups;
 using SCPSLBot.AI.FirstPersonControl.Perception.Senses;
 
 namespace SCPSLBot.AI.FirstPersonControl.Mind.Beliefs.Item.Keycard
 {
-    internal class KeycardWithinPickupDistance : ItemWithinPickupDistance<KeycardPickup>
+    internal class KeycardWithinPickupDistance : KeycardOfPermissions<ItemWithinPickupDistanceBase>
     {
-        public KeycardPermissions Permissions;
-        public KeycardWithinPickupDistance(KeycardPermissions permissions, ItemsWithinSightSense itemsSightSense)
+        public KeycardWithinPickupDistance(KeycardPermissions permissions, ItemsWithinSightSense itemsSightSense, ItemWithinPickupDistanceBase belief) 
+            : base(permissions, itemsSightSense, belief)
         {
-            this.Permissions = permissions;
-
-            _itemsSightSense = itemsSightSense;
-            _itemsSightSense.OnSensedItemWithinSight += ProcessSensedItem;
-            _itemsSightSense.OnAfterSensedItemsWithinSight += ProcessAbsentItem;
-        }
-
-        private readonly ItemsWithinSightSense _itemsSightSense;
-        private int numItems = 0;
-
-        private void ProcessSensedItem(ItemPickupBase item)
-        {
-            if (InventoryItemLoader.TryGetItem<KeycardItem>(item.Info.ItemId, out var keycard)
-                    && keycard.Permissions.HasFlag(Permissions))
-            {
-                if (!Item)
-                {
-                    Update(item);
-                }
-                numItems++;
-            }
-        }
-
-        private void ProcessAbsentItem()
-        {
-            if (numItems > 0)
-            {
-                if (Item)
-                {
-                    Update(null);
-                }
-                numItems = 0;
-            }
         }
     }
 }
---
+++ AI/FirstPersonControl/Mind/Beliefs/Item/KeycardOfPermissions.cs
@@ -0,0 +1,51 @@
+﻿using Interactables.Interobjects.DoorUtils;
+using InventorySystem;
+using InventorySystem.Items.Keycards;
+using InventorySystem.Items.Pickups;
+using SCPSLBot.AI.FirstPersonControl.Perception.Senses;
+
+namespace SCPSLBot.AI.FirstPersonControl.Mind.Beliefs.Item
+{
+    internal class KeycardOfPermissions<T> : ItemOfCriteriaBase<T> where T : ItemPickup<ItemPickupBase>
+    {
+        public KeycardPermissions Permissions;
+        public KeycardOfPermissions(KeycardPermissions permissions, ItemsWithinSightSense itemsSightSense, T belief) : base(belief)
+        {
+            this.Permissions = permissions;
+
+            _itemsSightSense = itemsSightSense;
+            _itemsSightSense.OnSensedItemWithinSight += ProcessSensedItem;
+            _itemsSightSense.OnAfterSensedItemsWithinSight += ProcessAbsentItem;
+        }
+
+        public static implicit operator T(KeycardOfPermissions<T> ofPermissions) => ofPermissions.belief;
+
+        private readonly ItemsWithinSightSense _itemsSightSense;
+        private int numItems = 0;
+
+        private void ProcessSensedItem(ItemPickupBase item)
+        {
+            if (InventoryItemLoader.TryGetItem<KeycardItem>(item.Info.ItemId, out var keycard)
+                    && keycard.Permissions.HasFlag(Permissions))
+            {
+                if (!belief.Item)
+                {
+                    belief.Update(item);
+                }
+                numItems++;
+            }
+        }
+
+        private void ProcessAbsentItem()
+        {
+            if (numItems > 0)
+            {
+                if (belief.Item)
+                {
+                    belief.Update(null);
+                }
+                numItems = 0;
+            }
+        }
+    }
+}
