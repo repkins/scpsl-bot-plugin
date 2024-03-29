121c5da6783484f1bae410a971ec69d5364beea3
Antons Repins <dumperix1993@gmail.com>
2024-03-29 12:12:11 +0200

    fix

--- AI/FirstPersonControl/Mind/Beliefs/Scp914/Scp914RunningOnSetting.cs
+++ AI/FirstPersonControl/Mind/Beliefs/Scp914/Scp914RunningOnSetting.cs
@@ -7,17 +7,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace SCPSLBot.AI.FirstPersonControl.Mind.Beliefs.Scp914
 {
     internal class Scp914RunningOnSetting : IBelief
     {
         public readonly Scp914KnobSetting Setting;
-        public Scp914RunningOnSetting(Scp914KnobSetting setting)
+        public Scp914RunningOnSetting(Scp914KnobSetting setting, FpcBotPlayer botPlayer)
         {
             this.Setting = setting;
+            this.botPlayer = botPlayer;
+
             EventManager.RegisterEvents(this);
         }
+
+        private readonly FpcBotPlayer botPlayer;
 
         public event Action OnUpdate;
 
@@ -31,6 +36,14 @@ namespace SCPSLBot.AI.FirstPersonControl.Mind.Beliefs.Scp914
             if (knobSetting == Setting)
             {
                 newRunningAtSetting = true;
+
+                var scp914Position = Scp914Controller.Singleton.transform.position;
+                var playerCamPosition = botPlayer.FpcRole.CameraPosition;
+
+                if (Vector3.Distance(scp914Position, playerCamPosition) > 10f)
+                {
+                    newRunningAtSetting = false;
+                }
             }
             else
             {
