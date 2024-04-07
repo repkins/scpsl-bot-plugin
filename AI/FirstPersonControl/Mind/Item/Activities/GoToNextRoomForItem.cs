6e708d7c959392d54501f53b5d15016b2dafb9bd
Antons Repins <dumperix1993@gmail.com>
2024-04-08 02:14:57 +0300

    next room for item introduction

---
+++ AI/FirstPersonControl/Mind/Item/Activities/GoToNextRoomForItem.cs
@@ -0,0 +1,38 @@
+﻿using SCPSLBot.AI.FirstPersonControl.Mind.Door;
+using SCPSLBot.AI.FirstPersonControl.Mind.Item.Beliefs;
+using System;
+
+namespace SCPSLBot.AI.FirstPersonControl.Mind.Item.Activities
+{
+    internal class GoToNextRoomForItem<C> : IActivity where C : IItemBeliefCriteria, IEquatable<C>
+    {
+        public readonly C Criteria;
+        public GoToNextRoomForItem(C criteria)
+        {
+            this.Criteria = criteria;
+        }
+
+        private NextRoomLocationForItem<C> nextRoomLocation;
+
+        public void SetEnabledByBeliefs(FpcMind fpcMind)
+        {
+            nextRoomLocation = fpcMind.ActivityEnabledBy<NextRoomLocationForItem<C>>(this, b => b.Criteria.Equals(Criteria), b => b.Position.HasValue);
+            fpcMind.ActivityEnabledBy<DoorObstacle>(this, b => !b.Is(nextRoomLocation.Position!.Value));
+        }
+
+        public void SetImpactsBeliefs(FpcMind fpcMind)
+        {
+            fpcMind.ActivityImpacts<ItemLocation<C>>(this, b => b.Criteria.Equals(Criteria));
+        }
+
+        public void Tick()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
