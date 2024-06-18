4545ce20ca1fb2272438028b50e29eb47d777ee4
Antons Repins <dumperix1993@gmail.com>
2024-06-18 20:15:42 +0300

    travelling on elevator wip

---
+++ AI/FirstPersonControl/Mind/Elevator/ElevatorObstacle.cs
@@ -0,0 +1,23 @@
+﻿using Interactables.Interobjects;
+using System;
+
+namespace SCPSLBot.AI.FirstPersonControl.Mind.Elevator
+{
+    internal class ElevatorObstacle : IBelief
+    {
+
+        public bool Has() => Elevator;
+
+        public ElevatorChamber Elevator { get; private set; }
+        public event Action OnUpdate;
+
+        private void Update(ElevatorChamber newChamberValue)
+        {
+            if (newChamberValue != Elevator) 
+            { 
+                Elevator = newChamberValue;
+                OnUpdate?.Invoke();
+            }
+        }
+    }
+}
---
+++ AI/FirstPersonControl/Mind/Elevator/TravelOnElevator.cs
@@ -0,0 +1,49 @@
+﻿using Interactables.Interobjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCPSLBot.AI.FirstPersonControl.Mind.Elevator
+{
+    internal class TravelOnElevator : IAction
+    {
+        private ElevatorObstacle elevatorObstacle;
+
+        public void SetEnabledByBeliefs(FpcMind fpcMind)
+        {
+        }
+
+        public void SetImpactsBeliefs(FpcMind fpcMind)
+        {
+            elevatorObstacle = fpcMind.ActionImpactsWithCondition<ElevatorObstacle>(this, b => !b.Has());
+        }
+
+        private readonly FpcBotPlayer botPlayer;
+
+        private TravelOnElevator(FpcBotPlayer botPlayer)
+        {
+            this.botPlayer = botPlayer;
+        }
+
+        public void Tick()
+        {
+            var elevatorChamber = elevatorObstacle.Elevator;
+            if (!elevatorChamber.IsReady)
+            {
+                return;
+            }
+
+            var elevatorGroup = elevatorChamber.AssignedGroup;
+            var targetLvl = (elevatorChamber.CurrentLevel + 1) % ElevatorDoor.AllElevatorDoors[elevatorGroup].Count;
+
+            var elevatorSyncMsg = new ElevatorManager.ElevatorSyncMsg(elevatorGroup, targetLvl);
+            botPlayer.BotHub.ConnectionToServer.Send(elevatorSyncMsg);
+        }
+
+        public void Reset()
+        {
+        }
+    }
+}
